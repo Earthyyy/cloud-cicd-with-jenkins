dfe8295a033257b9dbe3f391630ae6089bb3d281
Earthyy <elardi2001@gmail.com>
2024-03-12 11:25:18 +0000

    test: add dummy tests with junit

--- src/test/java/ma/insea/clouddemoapp/CloudDemoappApplicationTests.java
+++ src/test/java/ma/insea/clouddemoapp/CloudDemoappApplicationTests.java
@@ -2,6 +2,7 @@ package ma.insea.clouddemoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CloudDemoappApplicationTests {
@@ -9,5 +10,18 @@ class CloudDemoappApplicationTests {
     @Test
     void contextLoads() {
     }
+
+
+    @Test
+    void testThatWillSucceed() {
+        assertEquals(1, 1, "Dummy test failed as expected.");
+    }
+
+
+
+    // @Test
+    // public void testThatWillFail() {
+    //     assertEquals(1 , 2, "Dummy test failed as expected.");
+    // }
 
 }
